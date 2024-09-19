2aa332a1e552f78b87ffc38e9f84b8a87499a577
4rcher0116 <100230824+4rcher0116@users.noreply.github.com>
2024-09-19 10:41:43 -0500

    Sample Sign In Component with Background (Unstyled)

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import SignIn from '../components/LoginModules/Signin'
+import styles from './styles/_Login.module.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className={styles.mainbackground}>
+      <SignIn />
+    </div>
   )
 }
 
