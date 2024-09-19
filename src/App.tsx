22afc45b1c2fa06d57dc0ee7b689500111671387
4rcher0116 <100230824+4rcher0116@users.noreply.github.com>
2024-09-19 09:49:31 -0500

    Router Setup with Skeleton Components for Views

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './views/Dashboard';
+import Login from './views/Login';
+import Account from './views/Account';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/userAccount" element={<Account />} />
+      </Routes>
+    </Router>
   );
 }
 
