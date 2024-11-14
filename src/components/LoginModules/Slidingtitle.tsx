ebd5a13a31bb13b06d162f0230b14c43173e3a91
4rcher0116 <100230824+4rcher0116@users.noreply.github.com>
2024-11-14 14:03:34 -0600

    Login Screen Foundation

--- src/components/LoginModules/Slidingtitle.tsx
+++ src/components/LoginModules/Slidingtitle.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { CSSProperties } from "react";
 
-const Slidingtitle = () => {
+type SlidingTitleProps = {
+  text: string;
+  fontFamily?: string; 
+  color: CSSProperties["color"]
+};
+
+const SlidingTitle: React.FC<SlidingTitleProps> = ({ text, fontFamily, color }) => {
   return (
-    <div>Slidingtitle</div>
-  )
-}
+    <motion.h1
+      style={{ fontFamily: fontFamily, color: color, }}
+      initial={{ x: "100vw", opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ type: "spring", stiffness: 60, damping: 20 }}
+    >
+      {text}
+    </motion.h1>
+  );
+};
 
-export default Slidingtitle
+export default SlidingTitle;
+
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import SignIn from '../components/LoginModules/Signin'
 import styles from './styles/_Login.module.css'
+import TypewriterComponent from 'typewriter-effect'
+import wavingCharacter from '../assets/Character/waving_mascot.png'
 
 const Login = () => {
   return (
-    <div className={styles.mainbackground}>
-      <div className={styles.blackText}>Sliding Title here</div>
-      <SignIn />
+    <div className={styles.mainBackground}>
+       <div className={styles.backgroundOverlay}></div>
+      <div className={styles.allContentContainer}>
+        <div className={styles.leftContainer}>
+          <TypewriterComponent
+            options={{
+              autoStart: true,
+              loop: false,
+              delay: 75,
+              cursorClassName: styles.cursorStyle
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(
+                  `<span class="${styles.animatedText}">Cooking Companion.</span>`
+                )
+                .pauseFor(500)
+                .start();
+            }}
+          />
+
+          <div>
+            <img className={styles.mascotImage} src={wavingCharacter} alt='character' />
+          </div>
+        </div>
+
+        <SignIn />
+      </div>
     </div>
   )
 }
