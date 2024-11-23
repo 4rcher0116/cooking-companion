8cbc6ced10fa8d3b4f560f92ac89b787bb5e62c8
4rcher0116 <100230824+4rcher0116@users.noreply.github.com>
2024-11-23 13:37:51 -0600

    Quick filter menu stash

--- src/components/DashboardModules/RecipeDashboard.tsx
+++ src/components/DashboardModules/RecipeDashboard.tsx
@@ -10,4 +10,23 @@ const RecipeDashboard = () => {
   )
 }
 
+const FilterMenu = () => {
+  /**
+   * Filters to implement 
+   * Meal type - autocomplete - Options: breakfast, lunch, dinner, snack/dessert
+   * Max Cook time - autocompelte - Options: 'Quick(under 15 minutes)', 'Short(15-30 minutes)', 'Moderate (30-60 minutes)', 'Long (60-90 minutes)', 'Unlimited/No Limit', 
+   * Serving size - slider - 1-20
+   * Skill level - options: beginner, intermediate, advanced
+   * calories - autocomplete - Options: Low(under 200 calories), Moderate (under 400 calories), standard (under 600 calories), hearty ( under 800 calories), high (over 800 calories)
+   * Name search - text field
+ 
+  */
+  
+  return (
+    <div className={styles.filterMenuContainer}>
+      
+    </div>
+  )
+}
+
 export default RecipeDashboard
