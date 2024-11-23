a1507909a6928ea4cf30ef9d807630c0fd1d8a09
4rcher0116 <100230824+4rcher0116@users.noreply.github.com>
2024-11-23 11:56:30 -0600

    MUI packages

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import styles from './styles/_Dashboard.module.css'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className={styles.dashboardContent}>
+      <div className={styles.headerContent}>
+        <p className={styles.headerText}>Cooking Companion</p>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.recipeDashboardContainer}>Recipe Dash Placeholder</div>
+        <div className={styles.rightContainer}>
+          <div className={styles.animatedCharacterContainer}>
+            Animated Character Placeholder
+          </div>
+          <div>
+            Leaderboard Shortcut Placeholder
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
