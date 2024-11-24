397b25ab875cc9b60ff4968d4b2168089564dcb6
justwin02 <Jnguy129@gmail.com>
2024-11-24 16:04:41 -0600

    placeholder for pinned recipes on bookmark page

--- src/views/Bookmark.tsx
+++ src/views/Bookmark.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import styles from "./styles/_Bookmark.module.css"
+import { useNavigate } from 'react-router-dom'
 
 const Bookmark = () => {
+    const navigate = useNavigate();
+
+    const handleBacktoDashboard = () => {
+        navigate("../dashboard");
+    };
+
     return(
-        <div>Bookmark page</div>
-    )
-}
+        <div className={styles.bookmarkContainer}>
+            <div className={styles.headerContent}>
+                <div className={styles.headerText}>Bookmark</div>
+            </div>
+
+            <div className={styles.buttonContent}>
+                <button
+                    className={styles.buttonContainer}
+                    onClick={handleBacktoDashboard}
+                    >
+                        Back to Dashboard
+                </button>
+            </div>
+            <div className={styles.titleContent}>
+                <div className={styles.titleText}>Saved</div>
+            </div>
+            <div className= {styles.body}>
+                <div className={styles.bodyContent}>
+                    <div className={styles.container}> Placeholder for Saved Recipe</div>
+                    <div className={styles.container}> Placeholder for Saved Recipe</div>
+                    <div className={styles.container}> Placeholder for Saved Recipe</div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Bookmark;
