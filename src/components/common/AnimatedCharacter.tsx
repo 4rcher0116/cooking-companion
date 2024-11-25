7f29fe9e74f858bb014f8ce44fcc4cec33ef2488
4rcher0116 <100230824+4rcher0116@users.noreply.github.com>
2024-11-24 18:31:23 -0600

    Stash Before Restart

--- src/components/common/AnimatedCharacter.tsx
+++ src/components/common/AnimatedCharacter.tsx
@@ -1,5 +1,10 @@
 import TypewriterComponent from "typewriter-effect";
 import styles from "./styles/_AnimatedCharacter.module.css";
+import { ReactNode } from "react";
+import CookieIcon from "@mui/icons-material/Cookie";
+import IcecreamIcon from "@mui/icons-material/Icecream";
+import LocalPizzaIcon from "@mui/icons-material/LocalPizza";
+import LunchDiningIcon from "@mui/icons-material/LunchDining";
 
 type AnimatedCharacterProps = {
   sourceImage: string | undefined;
@@ -13,9 +18,7 @@ const AnimatedCharacter = ({
   return (
     <div className={styles.mascotContainer}>
       <img className={styles.mascotImage} src={sourceImage} alt="character" />
-      <div className={styles.iconBorder}>
-        <div className={styles.bottomLeft}></div>
-        <div className={styles.bottomRight}></div>
+      <IconBorder>
         <div className={styles.textContainer}>
           <TypewriterComponent
             options={{
@@ -35,7 +38,34 @@ const AnimatedCharacter = ({
             }}
           />
         </div>
-      </div>
+      </IconBorder>
+    </div>
+  );
+};
+
+type IconBorderProps = {
+  children: ReactNode;
+};
+
+const IconBorder = ({ children }: IconBorderProps) => {
+  return (
+    <div className={styles.iconBorder}>
+      {/* Top-left corner */}
+      <CookieIcon className={`${styles.cornerIcon} ${styles.topLeft}`} />
+
+      {/* Top-right corner */}
+      <LocalPizzaIcon className={`${styles.cornerIcon} ${styles.topRight}`} />
+
+      {/* Bottom-left corner */}
+      <LunchDiningIcon
+        className={`${styles.cornerIcon} ${styles.bottomLeft}`}
+      />
+
+      {/* Bottom-right corner */}
+      <IcecreamIcon className={`${styles.cornerIcon} ${styles.bottomRight}`} />
+
+      {/* Inner content */}
+      {children}
     </div>
   );
 };
